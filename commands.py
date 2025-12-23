330fdacb8fbac96ed0e848bfe88497255248eb94
megantebro <dazaizhongcun861@gmail.com>
2025-12-23 11:58:49 +0900

    インポートミス

--- commands.py
+++ commands.py
@@ -4,7 +4,7 @@ from os import name
 from discord import app_commands
 import discord
 
-import MessageData
+from MessageData import MessageData
 import ai
 from shared.db import get_avg_rank, get_avg_userscore, get_server_avg, get_server_stddev
 
@@ -128,7 +128,7 @@ async def ranking(interaction:discord.Interaction,worst:bool = False,limit:int =
 async def check(interaction:discord.Interaction,msg:str):
     await interaction.response.defer()
     msgData = MessageData(interaction.user,interaction.guild_id,msg)
-    score = (await ai.judge_message([msg]))[0]
+    score = (await ai.judge_message([msgData]))[0]
     await interaction.followup.send(f"発言は{score}点です")
 
 
